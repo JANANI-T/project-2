b47ec204d0c80defc4e62b4dc93cec971aaa8421
JANANI-T <jananisidh@gmail.com>
2017-07-20 09:32:29 +0530

    Create DescenOrder.java

---
+++ DescenOrder.java
@@ -0,0 +1,28 @@
+# project-2
+import java.util.Arrays;
+import java.util.Scanner;
+public class DescenOrder
+{
+public static void main(String args[])
+{
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter the size of array");
+	double size=s.nextDouble();
+	   
+		   double iAr[] =new double[(int) size];
+		   System.out.println("enter the element");
+		   for(int i=0;i<iAr.length;i++){
+			   iAr[i]=s.nextInt();
+		   }
+
+
+	   
+	   Arrays.sort(iAr);
+
+	   for(int j=iAr.length-1;j>=0;j--){
+		   System.out.print(iAr[j]+" ");
+	   }
+	   }
+
+	}
+
